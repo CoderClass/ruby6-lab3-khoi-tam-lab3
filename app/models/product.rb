84364bfbd812cbb47b23278b896ec0cd96fb44e0
Khoi Lai <khoi.geeky@gmail.com>
2017-06-28 19:42:03 +0700

    Milestone 4 WIP

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,13 @@ class Product < ActiveRecord::Base
   before_save :set_default_image_path
   validates :name, presence: true
 
+  def self.alphabetical
+    Product.order(:name)
+  end
   private
 
   def set_default_image_path
     self.image_path ||= "http://lorempixel.com/200/200/fashion"
   end
+
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".alphabetical" do
+    it "return empty if there is no product" do
+      expect(Product.alphabetical).to eq([])
+    end
+
+    it "must be sorted" do
+      %w(a b c).each {|name| Product.create(name: name) }
+      expect(Product.alphabetical.map(&:name)).to eq(["a","b","c"])
+    end
+  end
 end
